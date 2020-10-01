94a29c86dc97d4989d231e0de0ded64e08cdb7ab
Nery <nalbertoc132@gmail,com>
2020-10-01 00:00:04 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+/* eslint-disable react/jsx-closing-tag-location */
+import React from 'react'
+import { GlobalStyles } from './components/Styles/GlobalStyles'
+import { Logo } from './components/Logo'
+import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { NavBar } from './components/NavBar'
+import { Router } from '@reach/router'
+import Context from './Context'
+
+export const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Logo />
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+      </Router>
+      <Context.Consumer>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+              : <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router>
+
+        }
+      </Context.Consumer>
+
+      <NavBar />
+    </>
+  )
+}
---
+++ src/components/PhotoCard/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { ImgWrapper, Img, Article } from './styles'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
+import { FavButton } from '../FavButton'
+import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
+import { Link } from '@reach/router'
+
+const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
+
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const [show, element] = useNearScreen()
+  const key = `like-${id}`
+  const [liked, setLiked] = useLocalStorage(key, false)
+
+  return (
+    <Article ref={element}>
+      {show &&
+        <>
+          <Link href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </Link>
+          <ToggleLikeMutation>
+            {
+              (toggleLike) => {
+                const handleFavClick = () => {
+                  !liked && toggleLike({ variables: { input: { id } } })
+                  setLiked(!liked)
+                }
+                return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
+              }
+            }
+          </ToggleLikeMutation>
+        </>}
+    </Article>
+  )
+}
